ce9a1ea47df38d41ec5a0ba7618c699fedca737c
oleksandr <2356931k@gmail.com>
2022-06-25 12:32:15 +0300

    dasda

---
+++ Lesson01/index.js
@@ -0,0 +1,24 @@
+// Old Scripts
+// var n1 = 'n1';
+// n2 = 'n2';
+
+// var let const
+// New 
+const num = ['Sasha', 'Olha', 'Sveta'];
+let a = 2;
+let b = 2;
+let res = a + b;
+
+
+let test = ['Sasha', 'Olha', 'Sveta']
+
+//alert('test')
+
+//document.write('doc write')
+
+console.log(a)
+
+document.querySelector('.out').innerHTML = test;
+// window.addEventListener("load", function (event) {
+//     document.querySelector('.out').innerHTML = test;
+// });
