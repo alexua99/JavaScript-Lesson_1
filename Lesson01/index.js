2bc1732ee54a5e164d7f206d2eb5fc240ea7f441
oleksandr <2356931k@gmail.com>
2022-06-27 21:07:39 +0300

    add

--- Lesson01/index.js
+++ Lesson01/index.js
@@ -4,19 +4,24 @@
 
 // var let const
 // New 
-const num = ['Sasha', 'Olha', 'Sveta'];
+const userNameList = ['Sasha', 'Olha', 'Sveta'];
 let a = 2;
 let b = 2;
 let res = a + b;
 
 
-let test = ['Sasha', 'Olha', 'Sveta']
+
+let test = [
+    { name: "sasha", age: 18, },
+    { name: "Olha", age: 22, },
+    { name: "Sveta", age: 35, }
+]
 
 //alert('test')
 
 //document.write('doc write')
 
-console.log(a)
+console.log(test)
 
 document.querySelector('.out').innerHTML = test;
 // window.addEventListener("load", function (event) {
